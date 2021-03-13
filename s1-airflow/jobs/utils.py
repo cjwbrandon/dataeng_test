ad89d5f30feaf1d58b08145dbf75bcc82f3256d8
cjwbrandon <cjwbrandon@gmail.com>
2021-03-13 08:35:53 +0800

    [ADD][S1] Added function to split full names to first name and last name Added split_names function in s1-airflow/jobs/utils to split full names. Moved dataset1.csv and dataset2.csv to s1-airflow/data/. Add .gitignore file and ignore .ipynb_checkpoints

---
+++ s1-airflow/jobs/utils.py
@@ -0,0 +1,17 @@
+def split_names(names):
+    """Split full name to first and last name.
+    Note: Assumes that their last name is the last word.
+    
+    Parameters
+    ----------
+    names : pandas.Series
+        Full names to be splitted.
+        
+    Returns
+    -------
+    pandas.DataFrame
+    """
+
+    return names.str.rsplit(" ", n=1, expand=True).rename(
+        columns={0: "first_name", 1: "last_name"}
+    )
