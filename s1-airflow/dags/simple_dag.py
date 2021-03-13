6dea68b10916751882bde8e0a4ace99c592ce300
cjwbrandon <cjwbrandon@gmail.com>
2021-03-13 09:52:05 +0800

    [ADD][S1] Added dag to schedule task Shifted jobs and data folders into dags

---
+++ s1-airflow/dags/simple_dag.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+import jobs.dataset_job as dj
+
+# configurations
+default_args = {
+    "owner": "my_airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=30),
+}
+
+dag = DAG(
+    dag_id="simple_dag",
+    default_args=default_args,
+    description="Simple DAG to schedule processing tasks in Section 1.",
+    schedule_interval="0 1 * * *",
+)
+
+dataset_job = PythonOperator(task_id="dataset_job", python_callable=dj.main, dag=dag)
